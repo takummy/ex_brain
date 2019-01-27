bb4c5638c8979330b57702c0070f0b28c008ab97
Your Actual Name <Your Actual Email>
2019-01-27 20:22:50 +0900

    ログイン機能およびマイページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  resources :users, only: :show
 end
