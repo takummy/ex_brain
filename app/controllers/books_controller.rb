80c8800fe3cc2db80d40d1e81d800046cadaeda9
Your Actual Name <Your Actual Email>
2019-01-27 16:38:36 +0900

    book新規作成ページを作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,14 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :description, :image)
   end
 end
