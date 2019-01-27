2e273884a8f9bc6cbd23a1717a73773f095f4265
Your Actual Name <Your Actual Email>
2019-01-28 00:17:16 +0900

    Bookにuserの外部キーカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_26_020330) do
+ActiveRecord::Schema.define(version: 2019_01_27_151305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_01_26_020330) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "lesson_questions", force: :cascade do |t|
@@ -73,6 +75,7 @@ ActiveRecord::Schema.define(version: 2019_01_26_020330) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "books", "users"
   add_foreign_key "lesson_questions", "answers"
   add_foreign_key "lesson_questions", "lessons"
   add_foreign_key "lesson_questions", "questions"
