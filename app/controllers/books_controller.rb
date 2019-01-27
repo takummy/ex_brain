fec84ae36f254cadba83df47bceb68bd438d3e74
Your Actual Name <Your Actual Email>
2019-01-28 00:35:29 +0900

    書籍登録機能を作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,25 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
+    if @book.save
+      flash[:success] = '本を追加しました'
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
   end
+
+  def show
+    @book = Book.find[:params]
+  end
+
+
 
   private
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
     @user = User.find(params[:id])
+    @books = current_user.books
   end
 end
