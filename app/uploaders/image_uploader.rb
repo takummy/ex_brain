8b80e580d3b95fd50239e56fe8e7cca545b36dcc
Your Actual Name <Your Actual Email>
2019-01-28 01:04:42 +0900

    書籍の画像アップロード機能追加

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,40 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # def default_url(*args)
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  # process scale: [200, 300]
+
+  # def scale(width, height)
+  #   # do something
+  # end
+  
+  version :thumb do
+    process resize_to_fit: [200, 200]
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
+end
