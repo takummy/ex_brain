5221a2253677cfc1c6c2ecd3f9f88f5799041c9e
Your Actual Name <Your Actual Email>
2019-01-28 20:02:51 +0900

    ユーザーのカラム追加に伴い、deviseを修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -41,11 +41,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :name, :icon])
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :name, :icon])
   end
 
   def after_sign_up_path_for(resource)
