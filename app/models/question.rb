a94bbc50517d654de397f2ca922670e591a808b7
Your Actual Name <Your Actual Email>
2019-03-04 18:15:26 +0900

    問題作成時に正解が一つになるようバリデーションを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,15 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers
 
   validates :content, presence: true, length: { maximum: 200 }
+  validate :has_one_correct_answer
+
+  private
+
+  def has_one_correct_answer
+    if answers.map { |answer| answer.correct? || nil }.compact.count > 1
+      errors.add(:answers, "は一つ以上選択できません")
+    elsif answers.map { |answer| answer.correct? || nil }.compact.count == 0
+      errors.add(:answers, "を一つ選んでください")
+    end
+  end
 end
