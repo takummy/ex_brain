6bd230ba6705abcdb085e1bf4b2a01940c126a78
Your Actual Name <Your Actual Email>
2019-03-06 13:49:59 +0900

    fogを削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,11 +3,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
