9a5b67a43e9fffd515d4fe815dae9b2a5dbda8d1
Your Actual Name <Your Actual Email>
2019-05-27 13:40:25 +0900

    Lessonコントローラー、問題開始ボタンを作成

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,19 @@
+class LessonsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @lesson = current_user.lessons.build(lesson_params)
+    if @lesson.save
+      flash[:success] = "問題を開始します"
+      redirect_to root_path
+      # new_lesson_lesson_question_path(@lesson)
+    else
+      root_path
+    end
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:book_id)
+  end
+end
