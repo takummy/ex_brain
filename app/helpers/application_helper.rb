0bfd1c090ab2ec4b317145412938f380bf955dfd
Your Actual Name <Your Actual Email>
2019-05-29 13:38:38 +0900

    解答済みの場合、”前回の結果”ボタンを表示するよう変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def answered?(book_id)
     current_user.lessons.where(book_id: book_id).exists?
   end
+
+  def last_lesson(book_id)
+    current_user.lessons.where(book_id: book_id).pluck(:id)
+  end
 end
