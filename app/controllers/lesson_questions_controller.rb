bde62b4b0d2a60a79b0d6ad5a8c6f40ad392e8fa
Your Actual Name <Your Actual Email>
2019-06-12 23:46:01 +0900

    lesson_questions controllerをリファクタ close #41

--- app/controllers/lesson_questions_controller.rb
+++ app/controllers/lesson_questions_controller.rb
@@ -12,19 +12,10 @@ class LessonQuestionsController < ApplicationController
     lesson = Lesson.find(params[:lesson_id])
     lesson_question = lesson.lesson_questions.build(question_id: params[:question_id],
                                                     answer_id: params[:answer_id])
-    book = lesson.book
-    questions = book.questions
-    answered_questions = lesson.questions
 
     if lesson_question.save
       flash[:success] = "解答を保存しました"
-      next_question = questions - answered_questions
-
-      if next_question.any?
-        redirect_to new_lesson_lesson_question_path(lesson)
-      else
-        redirect_to lesson_lesson_questions_path
-      end
+      next_or_result(lesson)
     end
   end
 
@@ -32,5 +23,22 @@ class LessonQuestionsController < ApplicationController
     @lesson = Lesson.find(params[:lesson_id])
     @lesson_questions = @lesson.lesson_questions
     @result = @lesson.answers.map { |answer| answer.correct || nil }.compact.count
+  end
+
+  private
+  def has_next_question?(lesson)
+    book = lesson.book
+    questions = book.questions
+    answered_questions = lesson.questions
+    next_questions = questions - answered_questions
+    next_questions.any?
+  end
+
+  def next_or_result(lesson)
+    if has_next_question?(lesson)
+      redirect_to new_lesson_lesson_question_path(lesson)
+    else
+      redirect_to lesson_lesson_questions_path
+    end
   end
 end
